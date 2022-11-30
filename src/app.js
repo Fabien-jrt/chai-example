87599b14eb4091588d6f0d13d30f141ba32f1c89
Fabien-Jrt <67684689+Fabien-jrt@users.noreply.github.com>
2022-11-30 10:31:28 +0100

    Add Triangle.getArea implementation

--- src/app.js
+++ src/app.js
@@ -16,6 +16,20 @@ class Cube {
     }
 }
 
+class Triangle {
+    // angle in rad, length without specific unit
+    constructor(angle, len1, len2) {
+        this.angle = angle;
+        this.len1 = len1;
+        this.len2 = len2;
+    }
+    
+    getArea() {
+        return Math.round(0.5 * this.len1 * this.len2 * Math.sin(this.angle))
+    }
+}
+
 module.exports = {
-    Cube:Cube
+    Cube:Cube,
+    Triangle:Triangle
 }
