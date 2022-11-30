b383a01bf9abd0d7201d07465febdd767567dff9
Fabien-Jrt <67684689+Fabien-jrt@users.noreply.github.com>
2022-11-30 11:11:20 +0100

    Add Triangle.getSideLenght implementation

--- src/app.js
+++ src/app.js
@@ -27,6 +27,15 @@ class Triangle {
     getArea() {
         return Math.round(0.5 * this.len1 * this.len2 * Math.sin(this.angle))
     }
+    
+    getSideLength(side) {
+        if (side === 0)
+            return this.len1;
+        if (side === 1)
+            return this.len2;
+        // c = √b2 + a2 - 2ba·cos(C) = 25.52719
+        return Math.round(Math.sqrt(Math.pow(this.len2, 2) + Math.pow(this.len1, 2) - 2 * this.len2 * this.len1 * Math.cos(this.angle))) 
+    }
 }
 
 module.exports = {
